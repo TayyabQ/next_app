aad2f67ec2227ec6729bcc84a2f85bbed45f61b1
*Tayyab <*i200590@nu.edu.pk*>
2025-05-19 14:47:10 +0500

    Version 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NewsLetterPage from "./newsletter/page";
+import ContactPage from "./contact/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* {children} */}
+        <div className="flex flex-col lg:flex-row">
+          <NewsLetterPage/>
+          <ContactPage/>
+        </div>
       </body>
     </html>
   );
